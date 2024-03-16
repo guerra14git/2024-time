5b8061c22b3a0757bdc95d849ff0347631afe3d1
RGuerra775 <84286263+RGuerra775@users.noreply.github.com>
2024-03-16 17:28:56 +0000

    Add some extra features

--- pages/script.js
+++ pages/script.js
@@ -1,35 +1,81 @@
+const horas1 = document.getElementById('horas1');
+const minutos1 = document.getElementById('minutos1');
+const beer1 = document.getElementById('beer');
+const books1 = document.getElementById('books');
+const musics1 = document.getElementById('musics');
+
+// codigo para as frases
+
 const dataAlvo = new Date('2024-12-31').getTime();
 
 const interval = setInterval(function() {
+    // Consts. sobre as categorias de cada texto
     const agora = new Date().getTime();
     const diferenca = dataAlvo - agora;
     const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
-    
+    const horas = Math.floor(diferenca / (1000 * 60 * 60));
+    const minutos = Math.floor(diferenca / (1000 * 60));
+    const beer = Math.floor(96*(dias));
+    const meses = Math.floor((dataAlvo - agora) / (1000 * 60 * 60 * 24 * 30.44));
+    const books = Math.floor((meses)*2);
+    const musics = Math.floor(dias*480);
+
+    // frases do index.html
     document.getElementById('countdown').innerHTML = `${dias} days until the end <br> of 2024...`;
+    document.getElementById('countdown-h').innerHTML = `${horas} hours until <br> the end of 2024...`;
+    document.getElementById('countdown-m').innerHTML = `${minutos} minutes until <br> the end of 2024...`;
+    document.getElementById('beer').innerHTML = `${beer} beers until the end <br> of 2024... 🍺`;
+    document.getElementById('books').innerHTML = `${books} books read until the end <br> of 2024... 📖`;
+    document.getElementById('musics').innerHTML = `${musics} musics heard until <br> the end of 2024...🎵`;
     
+    // caso acabe o tempo
     if (diferenca < 0) {
         clearInterval(interval);
         document.getElementById('countdown').innerHTML = 'Times up!';
     }
-}, 1000);
+}, 1000);   
 
-function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-} // para verificar se há algum elemento não visivel
-
-function checkVisibility() {
-    if (isElementInViewport(countdown)) {
-        countdown.style.opacity = '1';
-        window.removeEventListener('scroll', checkVisibility);
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-window.addEventListener('scroll', checkVisibility);
+window.addEventListener('scroll', function() {
+    // consts
+    const horas1 = document.getElementById('countdown-h')
+    const minutos1 = document.getElementById('countdown-m')
+    var scrollTop = window.scrollY || window.pageYOffset; 
+    var windowHeight = window.innerHeight; 
+    var opacity = scrollTop / (windowHeight * 0.6); 
+  
+    // controlo opacidade
+    opacity = Math.min(1, Math.max(0, opacity));
 
-document.addEventListener('DOMContentLoaded', checkVisibility);
+    async function opacityIns() {
+    await sleep(100)
+    horas1.style.opacity = opacity;
+    await sleep(500);
+    minutos1.style.opacity = opacity;
+    await sleep(500);
+    beer1.style.opacity = opacity;
+    await sleep(500);
+    books1.style.opacity = opacity;
+    await sleep(500);
+    musics1.style.opacity = opacity; 
+    }
+
+    opacityIns();
+  });
+
+// codigo para o tamanho da tela
+
+function windowResize() {
+    // consts.
+    const screenWidth = window.innerWidth; 
+
+/* acabar*/
+}
+// chamar a função ao abrir a tela
+
+window.onload = windowResize;
+window.onresize = windowResize;
+
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// codigo para a frase
+
 const phrase = document.getElementById("phrase");
 const circle = document.getElementById("circle");
 
@@ -11,6 +13,25 @@ function sleep(ms) {
 }
 
 async function changePage() {
-    await sleep(400);  
+    await sleep(300);  
     window.location.href = './pages/index.html';
 }
+
+// codigo para a janela 
+
+function windowResize() {
+    // consts.
+    const screenWidth = window.innerWidth; 
+    const phrase = document.getElementById('phrase');
+    // if-else ´
+    
+    if (screenWidth < 768) {
+        phrase.style.fontSize = '30px';
+        phrase.innerHTML = `How much time <br> left in 2024?`;
+    } 
+    else {return 0;}
+}
+// chamar a função ao abrir a tela
+
+window.onload = windowResize;
+window.onresize = windowResize;
